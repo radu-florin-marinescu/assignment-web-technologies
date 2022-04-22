5b47a501de9a1814e05a8e738293d8fe5437a8ef
radu-florin-marinescu <verditesoftware@gmail.com>
2022-04-22 18:40:12 +0300

    Added bootstrap and jQuery.

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet">
+    <link href="libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="libraries/jquery-3.6.0.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="nofollow"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Display Data - PHP CRUD</title>
 </head>
 
 <body>
-    <p>Hello World</p>
+<p>Hello World</p>
 </body>
 
 </html>
