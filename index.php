d638226c7264ff48c61652b918694d1e2879d907
radu-florin-marinescu <verditesoftware@gmail.com>
2022-04-23 12:40:02 +0300

    Added fonts and started styling table.

--- index.php
+++ index.php
@@ -5,17 +5,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet">
+    <link href="styles/main.css" rel="stylesheet">
+    <link href="styles/fonts.css" rel="stylesheet">
+    <link href="styles/colors.css" rel="stylesheet">
     <link href="libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="libraries/font-awesome/css/font-awesome.min.css">
     <script src="libraries/jquery-3.6.0.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="nofollow"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Display Data - PHP CRUD</title>
+    <title>TableView</title>
 </head>
 
 <body>
-<p>Hello World</p>
+<div class="page-container background-grey">
+    <table>
+        <thead>
+        <tr>
+            <th style="text-transform:uppercase;">Nr.</th>
+            <th style="text-transform:uppercase;">ID Comanda</th>
+            <th style="text-transform:uppercase;">Client</th>
+            <th style="text-transform:uppercase;">Suma</th>
+            <th style="text-transform:uppercase;">Data</th>
+            <th style="text-transform:uppercase;">Plata</th>
+            <th style="text-transform:uppercase;">Manopera</th>
+            <th style="text-transform:uppercase;">% Manopera</th>
+            <th style="text-transform:uppercase;">Cost manopera</th>
+            <th style="text-transform:uppercase;">Nr. Produse</th>
+            <th style="text-transform:uppercase;">Status</th>
+            <th style="text-transform:uppercase;">Actiuni</th>
+        </tr>
+        </thead>
+    </table>
+</div>
 </body>
 
 </html>
